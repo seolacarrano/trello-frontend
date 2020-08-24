0e8c0c58e34bcfd4639c89a87b3f1119b5c03716
KVCobbs <kenyacobbs9001@gmail.com>
2020-08-24 12:15:58 -0400

    userlogin

---
+++ app.js
@@ -0,0 +1,44 @@
+const app = new Vue ({
+    el : '#app',         /* just So I don't forget el is just element */
+    data : {
+        loggedin: false, /* So the logic here is that based on the jwt token we recieve will switch the
+                            boolean of out code so the user will be logged in and be able to taken into their boards */
+
+        JwT : "",
+        createUN: "",
+        createPW: "",
+        devURL: "http://localhost:3000",
+        prodURL: null,
+        user: null,
+        toekn: null
+    },
+    methods: {
+            handleLogin: function() {
+
+            const URL = this.prodURL ? this.prodURL : this.devURL
+            const user = {username: this.createUN,password: this.createPW}
+            fetch(`${URL}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                this.user = data.user;
+                this.token = data.token;
+                this.loggedin = true;
+                this.createPW = ""
+                this.createUN = ""
+            });
+        },
+        handleLogOut: function() {
+            this.loggedin = false;
+            this.user=null;
+            this.token=null;
+        },
+    },
+});
+
+
