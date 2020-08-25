16ad2c862fe067e04d8e7a4ce3014f5246957f7e
Kristy Lee <leekristy01@gmail.com>
2020-08-25 12:30:00 -0400

    getting uerlogin

--- app.js
+++ app.js
@@ -11,18 +11,7 @@ const app = new Vue ({
         devURL: "http://localhost:3000",
         prodURL: null,
         user: null,
-        token: null,
-        boardSingle: false,
-        boards: [],
-        newBoard: "",
-        boardName:"",
-        boardID: 0,
-        // listResponse: false,
-        allLists: [],
-        listInput: "",
-        listID: 0,
-        allItems: [],
-        input:{}
+        token: null
     },
     methods: {
             handleLogin: function() {
@@ -52,6 +41,7 @@ const app = new Vue ({
         handleSignup: function(){
             const URL = this.prodURL ? this.prodURL : this.devURL;
             const user = { username: this.createUN, password: this.createPW };
+
             fetch(`${URL}/users`,{
             method: "POST",
             headers: {
@@ -67,136 +57,9 @@ const app = new Vue ({
                 alert("sign up successful");
             };
             });
-        },
-        getBoards: function(e){
-            const URL = this.prodURL ? this.prodURL : this.devURL
-            fetch(`${URL}/boards`, {
-                method: "get",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `bearer ${this.token}`
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                this.boards = data
-            })
-        },
-        showBoard: function(e){
-            const URL = this.prodURL ? this.prodURL : this.devURL
-            this.boardID = e.target.id
-            console.log(this.boardID, e.target.id)
-            fetch(`${URL}/boards/${e.target.id}`, {
-                method: "get",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `bearer ${this.token}`
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                this.boardSingle = true
-                this.boards = data.board_name
-                console.log(this.boards)
-            })
-            .then(() => {
-                this.showList()
-                this.showItems()
-            })
-        },
-       
-        createBoard: function(e){
-            const URL = this.prodURL ? this.prodURL : this.devURL
-            const newBoard = {board_name: this.boardName}
-            fetch(`${URL}/users/${this.user.id}/boards`, {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `bearer ${this.token}`
-                }, 
-                body: JSON.stringify(newBoard)
-            })
-            .then(response => response.json())
-            .then(data => {
-                this.boardSingle = false
-                this.getBoards()
-            })
-        },
-        showList: function(e){
-            const URL = this.prodURL ? this.prodURL : this.devURL
-            console.log(this.boardID)
-            fetch(`${URL}/boards/${this.boardID}/lists`, {
-                method: "get",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `bearer ${this.token}`
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                this.boardSingle = true
-                if (!data.response){
-                    this.allLists = data
-                    console.log(this.allLists)
-                }
-            })
-        },
-        createList: function(e){
-            const URL = this.prodURL ? this.prodURL : this.devURL
-            const list = {list_name: this.listInput}
-            fetch(`${URL}/boards/${this.boardID}/lists`, {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `bearer ${this.token}`
-                },
-                body: JSON.stringify(list)
-            })
-            .then(response => response.json())
-            .then(data => {
-                this.showList()
-                this.listInput=""
-            })
-        },
-        createItem: function(e){
-            const URL = this.prodURL ? this.prodURL : this.devURL
-            this.listID = e.target.id
-            console.log(this.listID)
-            const itemInput = this.input[this.listID]
-            console.log(itemInput)
-            const item = {item_name: itemInput}
-            fetch(`${URL}/boards/${this.boardID}/lists/${e.target.id}/items`, {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `bearer ${this.token}`
-                },
-                body: JSON.stringify(item)
-            })
-            .then(response => response.json())
-            .then(data => {
-                this.showItems()
-                this.itemInput = ""
-            })
-        },
-        showItems: function(e){
-            const URL = this.prodURL ? this.prodURL : this.devURL
-            fetch(`${URL}/boards/${this.boardID}/lists/${this.listID}/items`, {
-                method: "get",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `bearer ${this.token}`
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(this.listID)
-              this.allItems = data
-              console.log(this.allItems)
-            })
         }
+
     }
 });
+
+
